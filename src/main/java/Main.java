17f67ef7746c505e9671147382e97632c1f03754
Lolizer <ardnya@rambler.ru>
2019-07-04 11:17:43 +0300

    Main flaw edit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,16 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 
 //holds static global values on the server
-public class Main{
-    static ArrayList<Pair<String,String>> names = new ArrayList<>();//pairs (company,name)
-    static public void init(){
-        for(int i = 0; i < 10; i++)
-            Main.names.add(new Pair<>("100","Abrahaam"));
-            Main.names.add(new Pair<>("100","Jon"));
-            Main.names.add(new Pair<>("100","Max"));
-            Main.names.add(new Pair<>("200","Izergil"));
-            Main.names.add(new Pair<>("200","Alex"));
-            Main.names.add(new Pair<>("300","Josh"));
-            //etc
+public class Main {
+    static ArrayList<Pair<String, String>> names = new ArrayList<>();//pairs (company,name)
+
+    static public void init() {
+        Main.names.add(new Pair<>("100", "Abrahaam"));
+        Main.names.add(new Pair<>("100", "Jon"));
+        Main.names.add(new Pair<>("100", "Max"));
+        Main.names.add(new Pair<>("200", "Izergil"));
+        Main.names.add(new Pair<>("200", "Alex"));
+        Main.names.add(new Pair<>("300", "Josh"));
+        //etс
     }
 }
